4c1df8c27fbc5aa46fed26050a917a0b3f3cae35
Daniela Trulls <danielatrulls01@gmail.com>
2017-06-30 16:00:21 -0700

    Clean up code

--- js/scripts-interface.js
+++ js/scripts-interface.js
@@ -2,11 +2,9 @@ var Doctor = require('./../js/scripts.js').doctorDetails;
 var api = require('./../js/scripts.js').api;
 
 $(document).ready(function() {
-
    $('#search').click(function(){
     var uid = $("#userUid").val();
     console.log(uid);
-
     $.get(api + uid).then(function(response) {
       console.log(response);
       for (i = 0 ; i <= 14 ; i++){
@@ -22,8 +20,8 @@ $(document).ready(function() {
         $(".output").append("<li><span>" + doctor.name + "</span></br><div>" + doctor.location +"</br>" + doctor.specialities + "<img src='"+ doctor.image +"'></img></div></li>");
       };
     });
-
+  });
+  $("button").click(function(){
+    $("ul").empty();
   });
 });
-
-// headache
