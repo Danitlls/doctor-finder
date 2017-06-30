1c9414e23fd1fff599b94e861c93faf7ed1e2d11
Daniela Trulls <danielatrulls01@gmail.com>
2017-06-30 15:46:53 -0700

    Add bacis styling to header and cards

--- js/scripts-interface.js
+++ js/scripts-interface.js
@@ -9,17 +9,17 @@ $(document).ready(function() {
 
     $.get(api + uid).then(function(response) {
       console.log(response);
-      for (i = 0 ; i <= 19 ; i++){
-        var doctorName =  response.data[i].profile.first_name + " " +      response.data[i].profile.last_name;
+      for (i = 0 ; i <= 14 ; i++){
+        var doctorName =  response.data[i].profile.first_name + " " + response.data[i].profile.last_name;
         var doctorLocation = response.data[i].practices["0"].location_slug;
         var doctorSpecialities = response.data[i].specialties["0"].actor;
-
+        var image = response.data["0"].profile.image_url;
         //displays list in console:
-        var doctor = new Doctor(doctorName, doctorLocation, doctorSpecialities);
+        var doctor = new Doctor(doctorName, doctorLocation, doctorSpecialities, image);
         console.log(doctor);
 
-        //displays list in index.html
-        $(".output").append("<li>" + doctor.name + "</br>" + doctor.location +"</br>" + doctor.specialities + "</li>");
+        //displays list in index.html (could have done it without a constructor)
+        $(".output").append("<li><span>" + doctor.name + "</span></br><div>" + doctor.location +"</br>" + doctor.specialities + "<img src='"+ doctor.image +"'></img></div></li>");
       };
     });
 
--- js/scripts.js
+++ js/scripts.js
@@ -2,44 +2,14 @@
 var apiKey = require('./../.env').apiKey;
 
 // API constructor:
-exports.api = "https://api.betterdoctor.com/2016-03-01/doctors?location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=20&user_key=" + apiKey + "&query=";
-
+exports.api = "https://api.betterdoctor.com/2016-03-01/doctors?location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&&skip=0&limit=15&user_key=" + apiKey + "&query=";
 
 //Doctor constructor:
-var Doctor = function(name, location, specialities) {
+var Doctor = function(name, location, specialities, image) {
   this.name = name;
   this.location = location;
   this.specialities = specialities;
+  this.image = image;
 }
-
-// Api.prototype.getDoctors = function(){
-//   $.get('https://api.betterdoctor.com/2016-03-01/doctors?location=37.773,-122.413,100&skip=2&limit=10&user_key=' + apiKey).then(function(response) {
-//     console.log(response);
-
-//
-//
-// Api.prototype.getDoctors = function(){
-//   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query=toothache&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=' + apiKey).then(function(response) {
-//     console.log(response);
-//     // var doctor;
-//     // // console.log(response);
-//     // // if (response.data[i].specialties["0"].actor = $("#userUid"))
-//     // for (i = 0 ; i <= 9 ; i++){
-//     //   var doctorName =  response.data[i].profile.first_name + " " +      response.data[i].profile.last_name;
-//     //   var doctorLocation = response.data[i].practices["0"].location_slug;
-//     //   var doctorSpecialities = response.data[i].specialties["0"].actor;
-//     //
-//     //
-//     //   // var doctor[i] = new Doctor(response.data[i].profile.first_name , response.data[i].practices["0"].location_slug , response.data[i].specialties["0"].actor );
-//     //   //
-//     //
-//     //   // console.log(doctorName, doctorLocation, doctorSpecialities  );
-//     //   doctor = new Doctor(doctorName, doctorLocation, doctorSpecialities);
-//     //   return doctor;
-//     //   console.log(doctor);
-//     // };
-//   });
-// }
-
 
 exports.doctorDetails =  Doctor ;
