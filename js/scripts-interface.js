f5355dabc40132adbb71b79b47285b2eb4c8da04
Daniela Trulls <danielatrulls01@gmail.com>
2017-06-30 14:36:45 -0700

    It shows the doctors base on user medical issue, return results in index.html and console

--- js/scripts-interface.js
+++ js/scripts-interface.js
@@ -1,21 +1,29 @@
-var Doctor = require('./../js/scripts.js').doctorList;
-var Api = require('./../js/scripts.js').apiInfo;
-
+var Doctor = require('./../js/scripts.js').doctorDetails;
+var api = require('./../js/scripts.js').api;
 
 $(document).ready(function() {
-  // var uid = "cold";
 
    $('#search').click(function(){
     var uid = $("#userUid").val();
-    var ndoc= new Api;
-    console.log(ndoc.getDoctors());
     console.log(uid);
+
+    $.get(api + uid).then(function(response) {
+      console.log(response);
+      for (i = 0 ; i <= 19 ; i++){
+        var doctorName =  response.data[i].profile.first_name + " " +      response.data[i].profile.last_name;
+        var doctorLocation = response.data[i].practices["0"].location_slug;
+        var doctorSpecialities = response.data[i].specialties["0"].actor;
+
+        //displays list in console:
+        var doctor = new Doctor(doctorName, doctorLocation, doctorSpecialities);
+        console.log(doctor);
+
+        //displays list in index.html
+        $(".output").append("<li>" + doctor.name + "</br>" + doctor.location +"</br>" + doctor.specialities + "</li>");
+      };
+    });
+
   });
-
-    // var nearbyDoctors = new Doctor(nearbyDoctorsUid);
-
-
-    // console.log(nearbyDoctors);
-    // console.log(nearbyDoctorsUid);
-
 });
+
+// headache
--- js/scripts.js
+++ js/scripts.js
@@ -2,44 +2,44 @@
 var apiKey = require('./../.env').apiKey;
 
 // API constructor:
-var Api = function(x){
-  this.apiK = x;
-}
+exports.api = "https://api.betterdoctor.com/2016-03-01/doctors?location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=20&user_key=" + apiKey + "&query=";
+
 
 //Doctor constructor:
-var Doctor = function(name, location, uid) {
+var Doctor = function(name, location, specialities) {
   this.name = name;
   this.location = location;
-  this.uid = uid;
+  this.specialities = specialities;
 }
 
 // Api.prototype.getDoctors = function(){
 //   $.get('https://api.betterdoctor.com/2016-03-01/doctors?location=37.773,-122.413,100&skip=2&limit=10&user_key=' + apiKey).then(function(response) {
 //     console.log(response);
 
+//
+//
+// Api.prototype.getDoctors = function(){
+//   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query=toothache&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=' + apiKey).then(function(response) {
+//     console.log(response);
+//     // var doctor;
+//     // // console.log(response);
+//     // // if (response.data[i].specialties["0"].actor = $("#userUid"))
+//     // for (i = 0 ; i <= 9 ; i++){
+//     //   var doctorName =  response.data[i].profile.first_name + " " +      response.data[i].profile.last_name;
+//     //   var doctorLocation = response.data[i].practices["0"].location_slug;
+//     //   var doctorSpecialities = response.data[i].specialties["0"].actor;
+//     //
+//     //
+//     //   // var doctor[i] = new Doctor(response.data[i].profile.first_name , response.data[i].practices["0"].location_slug , response.data[i].specialties["0"].actor );
+//     //   //
+//     //
+//     //   // console.log(doctorName, doctorLocation, doctorSpecialities  );
+//     //   doctor = new Doctor(doctorName, doctorLocation, doctorSpecialities);
+//     //   return doctor;
+//     //   console.log(doctor);
+//     // };
+//   });
+// }
 
 
-Api.prototype.getDoctors = function(){
-  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query=toothache&skip=0&limit=10&user_key=' + apiKey).then(function(response) {
-    console.log(response);
-    // var doctor;
-    // // console.log(response);
-    // // if (response.data[i].specialties["0"].actor = $("#userUid"))
-    // for (i = 0 ; i <= 9 ; i++){
-    //   var doctorName =  response.data[i].profile.first_name + " " +      response.data[i].profile.last_name;
-    //   var doctorLocation = response.data[i].practices["0"].location_slug;
-    //   var doctorSpecialities = response.data[i].specialties["0"].actor;
-    //
-    //
-    //   // var doctor[i] = new Doctor(response.data[i].profile.first_name , response.data[i].practices["0"].location_slug , response.data[i].specialties["0"].actor );
-    //   //
-    //
-    //   // console.log(doctorName, doctorLocation, doctorSpecialities  );
-    //   doctor = new Doctor(doctorName, doctorLocation, doctorSpecialities);
-    //   return doctor;
-    //   console.log(doctor);
-    // };
-  });
-}
-exports.apiInfo =  Api ;
-exports.doctorList =  Doctor ;
+exports.doctorDetails =  Doctor ;
