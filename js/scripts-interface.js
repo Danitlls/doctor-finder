f7d2bf50eba3b5c9cfab1f1253c1f08bf3ec785b
Daniela Trulls <danielatrulls01@gmail.com>
2017-06-30 12:21:37 -0700

    Use Doctor constructor to create new doctors displaying selected information in console

--- js/scripts-interface.js
+++ js/scripts-interface.js
@@ -1,15 +1,16 @@
 var Doctor = require('./../js/scripts.js').doctorList;
 var Api = require('./../js/scripts.js').apiInfo;
 
+var uid ;
 
 $(document).ready(function() {
   // var uid = "cold";
    $('#search').click(function(){
-    var uid = $("#userUid").val();
+    uid = $("#userUid").val();
     var nearbyDoctors = new Doctor(uid);
     var nearbyDoctorsUid = new Api(uid);
     console.log(uid);
-    console.log(nearbyDoctors.doctorFinder());
+    console.log(nearbyDoctors);
     console.log(nearbyDoctorsUid.getUid());
   });
 });
--- js/scripts.js
+++ js/scripts.js
@@ -2,42 +2,41 @@
 var apiKey = require('./../.env').apiKey;
 
 //constructor
+
+//
+// Doctor.prototype.doctorFinder = function(){
+//   return Doctor.name;
+// }
+
+var Api = function(x){
+  this.apiK = x;
+}
 var Doctor = function(name, location, uid) {
   this.name = name;
   this.location = location;
-  this.uid = uid
+  this.uid = uid;
 }
 
-Doctor.prototype.doctorFinder = function(){
-  return Doctor.name;
-}
-
-var Api = function(x){
-  this.userUid = x;
-}
 
 Api.prototype.getUid = function(){
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?location=37.773,-122.413,100&skip=2&limit=10&user_key=' + apiKey).then(function(response) {
 
-    console.log(response);
+    // console.log(response);
+    // if (response.data[i].specialties["0"].actor = $("#userUid"))
     for (i = 0 ; i <= 9 ; i++){
-      console.log(response.data[i].profile.first_name + " " + response.data[i].profile.last_name);
-    }
+      var doctorName =  response.data[i].profile.first_name + " " +      response.data[i].profile.last_name;
+      var doctorLocation = response.data[i].practices["0"].location_slug;
+      var doctorSpecialities = response.data[i].specialties["0"].actor;
 
+
+      // var doctor[i] = new Doctor(response.data[i].profile.first_name , response.data[i].practices["0"].location_slug , response.data[i].specialties["0"].actor );
+      //
+
+      // console.log(doctorName, doctorLocation, doctorSpecialities  );
+      var doctor = new Doctor(doctorName, doctorLocation, doctorSpecialities);
+      console.log(doctor);
+    };
   });
-};
-
+}
 exports.apiInfo =  Api ;
 exports.doctorList =  Doctor ;
-
-
-
-// var ExampleModule = function(args) {
-//   this.args = args; //to be replaced with constructor arguments
-// };
-//
-// ExampleModule.prototype.examplePrototype = function() {
-//   return `this is an example prototype method`;
-// };
-//
-// exports.exampleModule = ExampleModule;
